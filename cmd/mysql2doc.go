2f5fe8b03ac6cf5c2c7298b4359eda1894cc0d81
guoridgepole <>
2023-03-04 18:27:03 +0800

    定义接收参数

--- cmd/mysql2doc.go
+++ cmd/mysql2doc.go
@@ -1,22 +1,40 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/spf13/cobra"
 )
+
+// 数据库host地址
+var host string
+
+// 数据库端口
+var port string
+
+// 数据库用户名
+var user string
+
+// 数据库密码
+var password string
+
+// 数据库名称
+var dbname string
+
+// 表名称，支持使用下划线或百分号进行模糊匹配
+var tablename string
+
+// 输出的文件格式，为空则默认为md(markdown)
+var fileType string
+
+// 输出的文件名称
+var fileName string
 
 // 定义实现mysql2doc子命令
 var mysql2docCmd = &cobra.Command{
 	Use:   "mysql2doc",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "mysql数据库表结构文档生成工具",
+	Long:  "该工具可以根据mysql表结构，生成对应的数据库文档，支持输出markdown和word两种格式",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("mysql2doc called")
+
 	},
 }
 
@@ -32,4 +50,13 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// mysql2docCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	mysql2docCmd.Flags().StringVarP(&host, "host", "H", "localhost", "数据库host地址 默认为本机地址")
+	mysql2docCmd.Flags().StringVarP(&port, "port", "P", "3306", "数据库端口 默认为3306")
+	mysql2docCmd.Flags().StringVarP(&user, "user", "u", "root", "数据库用户名 默认为root")
+	mysql2docCmd.Flags().StringVarP(&password, "password", "p", "", "数据库密码")
+	mysql2docCmd.Flags().StringVarP(&dbname, "dbname", "d", "", "数据库名称")
+	mysql2docCmd.Flags().StringVarP(&tablename, "tablename", "t", "", "表名称，支持使用下划线或百分号进行模糊匹配，不传入则生成所有表")
+	mysql2docCmd.Flags().StringVarP(&fileType, "filetype", "T", "md", "输出的文件格式，md为markdown，不输入则默认为markdown格式")
+	mysql2docCmd.Flags().StringVarP(&fileName, "filename", "f", "", "输出的文件名称")
 }
